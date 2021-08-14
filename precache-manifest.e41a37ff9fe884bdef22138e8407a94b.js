d3c4d3245ad8a86bceb884285c23af68507cc59b
YuliyaBaskakova3012 <juliabaskakova3012@gmail.com>
2021-08-14 19:23:14 +0300

    Updates

---
+++ precache-manifest.e41a37ff9fe884bdef22138e8407a94b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a97bf3be79cf58b130afd04b50677c",
+    "url": "/YuliyaBaskakova3012/demo-social-network/index.html"
+  },
+  {
+    "revision": "abf09f4724b794fc2252",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/css/3.35ac9b18.chunk.css"
+  },
+  {
+    "revision": "0273ae80b82ef7b35bc6",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/css/4.596d3af4.chunk.css"
+  },
+  {
+    "revision": "b221b1d0910e66603a3c",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/css/main.d8373f00.chunk.css"
+  },
+  {
+    "revision": "57c638e90bfde4aa1d24",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/js/2.73383c46.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/js/2.73383c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf09f4724b794fc2252",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/js/3.15cfc238.chunk.js"
+  },
+  {
+    "revision": "0273ae80b82ef7b35bc6",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/js/4.d982cfdc.chunk.js"
+  },
+  {
+    "revision": "b221b1d0910e66603a3c",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/js/main.d719a8c0.chunk.js"
+  },
+  {
+    "revision": "85f4b7ab29b64172ad3e",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/js/runtime-main.4d1a5fc3.js"
+  },
+  {
+    "revision": "14707564c3a7e767c76d0aa5fb2a6585",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/media/load.14707564.gif"
+  },
+  {
+    "revision": "f4f3dd7acfd64b16aa51027159a6f5e8",
+    "url": "/YuliyaBaskakova3012/demo-social-network/static/media/user.f4f3dd7a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo-social-network/precache-manifest.fd6e306b2af7d77f25b73cd52c9c68da.js"
+  "/YuliyaBaskakova3012/demo-social-network/precache-manifest.e41a37ff9fe884bdef22138e8407a94b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo-social-network/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YuliyaBaskakova3012/demo-social-network/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
