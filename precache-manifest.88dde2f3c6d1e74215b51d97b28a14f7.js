03dd199ec946f7dd8a32faa183ea2050f85098ac
YuliyaBaskakova3012 <juliabaskakova3012@gmail.com>
2021-08-15 18:36:54 +0300

    Updates

--- precache-manifest.88dde2f3c6d1e74215b51d97b28a14f7.js
+++ precache-manifest.88dde2f3c6d1e74215b51d97b28a14f7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eaef63cd413728401cf1a4552ea6bcb1",
+    "revision": "ef30b0d1a38a879748fc7cbfcdeefdff",
     "url": "/demo-social-network/index.html"
   },
   {
@@ -8,36 +8,36 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/demo-social-network/static/css/3.35ac9b18.chunk.css"
   },
   {
-    "revision": "0273ae80b82ef7b35bc6",
+    "revision": "0d97498fa7f6a6f67747",
     "url": "/demo-social-network/static/css/4.596d3af4.chunk.css"
   },
   {
-    "revision": "47c6f8822f05d64d9117",
+    "revision": "c33e55a3310d9455675c",
     "url": "/demo-social-network/static/css/main.d8373f00.chunk.css"
   },
   {
-    "revision": "57c638e90bfde4aa1d24",
-    "url": "/demo-social-network/static/js/2.73383c46.chunk.js"
+    "revision": "5c2df9cfb7f2846a81f6",
+    "url": "/demo-social-network/static/js/2.b0d57818.chunk.js"
   },
   {
     "revision": "774152ec3dfb5dadc6880f2e7f79e400",
-    "url": "/demo-social-network/static/js/2.73383c46.chunk.js.LICENSE.txt"
+    "url": "/demo-social-network/static/js/2.b0d57818.chunk.js.LICENSE.txt"
   },
   {
     "revision": "abf09f4724b794fc2252",
     "url": "/demo-social-network/static/js/3.15cfc238.chunk.js"
   },
   {
-    "revision": "0273ae80b82ef7b35bc6",
-    "url": "/demo-social-network/static/js/4.d982cfdc.chunk.js"
+    "revision": "0d97498fa7f6a6f67747",
+    "url": "/demo-social-network/static/js/4.0bd76bb6.chunk.js"
   },
   {
-    "revision": "47c6f8822f05d64d9117",
-    "url": "/demo-social-network/static/js/main.ce43d84b.chunk.js"
+    "revision": "c33e55a3310d9455675c",
+    "url": "/demo-social-network/static/js/main.51cdddae.chunk.js"
   },
   {
-    "revision": "f2a2452d9973a8ee562a",
-    "url": "/demo-social-network/static/js/runtime-main.259f9950.js"
+    "revision": "30137c0b9d7a33b955de",
+    "url": "/demo-social-network/static/js/runtime-main.a15baacc.js"
   },
   {
     "revision": "14707564c3a7e767c76d0aa5fb2a6585",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo-social-network/precache-manifest.722fef4bdae941b2d189cf5ed53b37e0.js"
+  "/demo-social-network/precache-manifest.88dde2f3c6d1e74215b51d97b28a14f7.js"
 );
 
 self.addEventListener('message', (event) => {
